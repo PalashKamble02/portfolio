b05a3d19cd30f0a21bd3eba653e0ab2124d0ede3
newlearner2407 <manojbajpeyi804@gmail.com>
2024-06-25 22:29:13 +0530

    Created base

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 
 const App = () => {
   return (
     <div>
-      
+      <>
+        <Navbar />
+        <div className="container">
+          <Home />
+        </div>
+      </>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+    <div className="container home">
+       <div className="left">
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, aut.</h1>
+        
+        <a href="" className="btn btn-outline-warning">Download Resume</a>
+        </div>
+        <div className="right">
+            </div> 
+    </div>
+    </>
+  )
+}
+
+export default Home
